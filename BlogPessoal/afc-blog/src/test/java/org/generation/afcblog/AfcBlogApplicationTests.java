8bff5b1ec94087eab88fbf5f47c1a35126050a87
unknown <anaflaviacamelorc@gmail.com>
2021-08-01 17:09:48 -0300

    Blog Pessoal

---
+++ BlogPessoal/afc-blog/src/test/java/org/generation/afcblog/AfcBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.afcblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfcBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
